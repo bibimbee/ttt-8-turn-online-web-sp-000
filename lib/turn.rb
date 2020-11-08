5d404d417fb2ff1fd915d7ddb7708870b04e366b
Emma Danica <ms.bee.indeed@gmail.com>
2020-11-08 20:55:38 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,3 +1,13 @@
+def display_board(board)
+
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  board = [" "," "," "," "," "," "," "," "," "]
+
+  end
 def valid_move?(board,index)
 def position_taken?(board, index)
   if board[index] == " " || board[index] == "" || board[index] == nil
