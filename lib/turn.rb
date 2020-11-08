340384f20d048faff1deab1bc14d96ddc4f1eaf7
Emma Danica <ms.bee.indeed@gmail.com>
2020-11-08 20:57:58 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,28 +6,28 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
   board = [" "," "," "," "," "," "," "," "," "]
-end
-
-def valid_move?(board,index)
-def position_taken?(board, index)
-  if board[index] == " " || board[index] == "" || board[index] == nil
-    return false
-  else
-    return true
+  
+  def valid_move?(board,index)
+  def position_taken?(board, index)
+    if board[index] == " " || board[index] == "" || board[index] == nil
+      return false
+    else
+      return true
+    end
   end
-end
 
-def on_board?(index)
-  if index.between?(0,8) == true
+  def on_board?(index)
+    if index.between?(0,8) == true
+      return true
+    else
+      return false
+    end
+  end
+
+  if (position_taken?(board,index)) == false && (on_board?(index)) == true
     return true
   else
     return false
   end
-end
-
-if (position_taken?(board,index)) == false && (on_board?(index)) == true
-  return true
-else
-  return false
-end
+  end
 end
